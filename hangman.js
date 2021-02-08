67ce92c739533ecefdd54a3fa5437f41f65ef154
Matt Freebery <74575157+mfreebery@users.noreply.github.com>
2021-02-08 15:53:45 -0500

    added action plan

--- hangman.js
+++ hangman.js
@@ -7,3 +7,19 @@ guessed, the player gets to guess a new letter
 player guessed, a new portion of the hangman is added
 4. if the hangman is assembled completely before the 
 word or phrase is filled out, the player loses. 
+
+- Action Plan -
+1. Create array of possible words and pick one
+-loop through the words to identify special game word 
+-create an array of letters guessed correctly
+-create an array of letters guessed incorrectly
+-limit number of guesses to 6 
+2. ask user to pick letter
+-check to see if special word contains chosen letter
+-display if the letter is or is not in special word
+-display how many remaining chances player has
+3. diplay game state on screen
+-display current word status
+-display guessed letters that were incorrect 
+4 Continue until game over
+-ask if player would like to play again
