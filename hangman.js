d13beb96edff66314ced3eb09d0119c631c15b56
Matt Freebery <74575157+mfreebery@users.noreply.github.com>
2021-02-08 15:38:38 -0500

    added rules of hangman

---
+++ hangman.js
@@ -0,0 +1,9 @@
+/* 
+- Rules of hangman -
+1. a letter of the alphabet is guessed by the player
+2. if the word or phrase contains the letter the player
+guessed, the player gets to guess a new letter
+3. if the word or phrase does not contain the letter the 
+player guessed, a new portion of the hangman is added
+4. if the hangman is assembled completely before the 
+word or phrase is filled out, the player loses. 
